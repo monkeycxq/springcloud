2ff537dcc2b8f921312d24e04b39560b7c5b02b5
陈小强 <18801731538@163.com>
2021-04-09 09:39:04 +0800

    使用nacos替换eureka ;

---
+++ nacos-config/src/main/java/com/example/nacosconfig/NacosConfigApplication.java
@@ -0,0 +1,17 @@
+package com.example.nacosconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @authoe cxq
+ * @date 2021/4/7
+ */
+@SpringBootApplication
+public class NacosConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class, args);
+    }
+
+}
